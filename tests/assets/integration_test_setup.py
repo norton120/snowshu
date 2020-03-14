d77045b015b4b177119011945475d0a6e4f1cbb7
ethan knox <ethan.m.knox@gmail.com>
2020-03-14 19:10:15 -0400

    added bq to script

--- tests/assets/integration_test_setup.py
+++ tests/assets/integration_test_setup.py
@@ -1,57 +1,78 @@
 #!/usr/local/python3
 import pandas as pd
+from google.cloud import bigquery
+from google.oauth2 import service_account
 from sqlalchemy import create_engine
 import yaml
 import os
 import sys
 
-PROJECT_ROOT = os.cwd()
+PROJECT_ROOT = os.getcwd()
 
 
 def collect_tables():
     files=list()
     tables=set()
-    for root,dirs,files in os.walk(os.path.join(PROJECT_ROOT,
+    for root,dirs,found_files in os.walk(os.path.join(PROJECT_ROOT,
                                                 "tests",
                                                 "assets",
                                                 "data")):
-        for name in files:
+        print(f'traversing {root} for files...')
+        for name in found_files:
             if name.endswith('.csv'):
                 files.append(os.path.join(PROJECT_ROOT,
                                           "tests",
                                           "data",
                                           root,
                                           name))
+    print(f'found {len(files)} files.')
     for f in files:
         fileparts=f.split(os.path.sep)
         for part in fileparts:
             if part.startswith('SCHEMA='):
                 schema=part.replace('SCHEMA=','')
         name=fileparts[-1].replace('.csv','')
-        tables.add((f,schema,name,))
+        tables.add((f,schema.lower(),name.lower(),))
+    print(f'found {len(tables)} tables.')
     return tables
 
-def get_conn(profile, creds_file):
-    with open(creds_file, 'r') as f:
-        creds=yaml.loads(f.read())['sources'][profile]
+def get_conn(profile, keyfile, database):
+
         if profile == 'bigquery':
-            return create_engine('bigquery://SNOWSHU-DEVELOPMENT',
-                                 credentials_path=creds['keyfile_path']
-                                )
+            return create_engine(f'bigquery://{database}', credentials_path=keyfile)
         if profile == 'snowflake':
-            return create_engine('snowflake:://{}:{}@{}/SNOWSHU_DEVELOPMENT'.format((creds['user'],
+            return create_engine('snowflake://{}:{}@{}/{}'.format((creds['user'],
     creds['password'],
-    creds['account'],)
+    creds['account'],
+    database,)
 ))
         else:
             raise ValueError(f'{profile} not supported by setup script.')
 
 def main(profile,
          creds_file):
-        
-    database="SNOWSHU_DEVELOPMENT" 
+    print(f'executing seed for {profile} with creds file {creds_file}')        
+    database='snowshu-development' if profile == 'bigquery' else 'SNOWSHU_DEVELOPMENT' 
+    with open(creds_file, 'r') as f:
+        all_creds=yaml.load(f.read())['sources']
+        keyfile=[x for x in all_creds if x['name'] == profile][0]['keyfile_path']
+    print('collecting tables...')
     tables=collect_tables()
-    conn=get_conn(profile,creds_file)
+    print(f'collected {len(tables)} tables.')
+
+    if profile == 'bigquery':
+        print('connecting to bigquery...')
+        client = bigquery.Client(project=database,
+                                 credentials=service_account.Credentials.from_service_account_file(keyfile))
+        print('connected')
+        for dataset in set([x[1] for x in tables]):
+            print(f'creating dataset {dataset} in bigquery..')
+            dataset = bigquery.Dataset(f"{database}.{dataset}")
+            dataset.location = "US"
+            client.create_dataset(dataset)
+            print('created')
+    conn=get_conn(profile,keyfile,database)
+    print(f'created connection {conn}')
 
     for table in tables:
         print(f'Creating table {table[2]}...')     
@@ -63,16 +84,16 @@ def main(profile,
         print(f'Created table {table[2]}.')     
 
     ## make views 
-    users_view=dict(name='USERS_VIEW',schema='SOURCE_SYSTEM',sql='SELECT * FROM "SNOWSHU_DEVELOPMENT"."SOURCE_SYSTEM"."USERS"')
-    address_region_attributes_view=dict(name='address_region_attributes_view',schema='EXTERNAL_DATA',sql='SELECT * FROM "SNOWSHU_DEVELOPMENT"."EXTERNAL_DATA"."ADDRESS_REGION_ATTRIBUTES"')
+    users_view=dict(name='users_view',schema='source_system',sql=f'select * from {database}.source_system.users')
+    address_region_attributes_view=dict(name='address_region_attributes_view',schema='external_data',sql=f'select * from {database}.external_data.address_region_attributes')
     
     for view in (users_view,address_region_attributes_view,):
         logger.info(f'Creating view {view.name}...')
-        create=f'CREATE VIEW "SNOWSHU_DEVELOPMENT"."{view.schema}"."{view.name}" AS {view.sql}'
+        create=f'CREATE VIEW {database}.{view.schema}.{view.name} AS {view.sql}'
         conn.execute(create)
         logger.info('Done creating view {view.name}.')
 
 if __name__ == "__main__":
-    main(sys.argval[1:])
+    main(*sys.argv[1:])
 
 
